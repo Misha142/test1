7efb6346109aa84b87673b6cbfa28e37cdb90f0b
Mishanya <mishastefanovich@mail.ru>
2023-07-25 20:11:20 +0300

    написал программу

--- test/Program.cs
+++ test/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+string[] array = { "Hello,", "2,", "world,", "1234,", "1567,", "123,", "qws", "+_)", "computer science,","1" };
+PrintArray(array);
+WriteLine();
+string[] outputArray = CreateNewArray(array);
+PrintArray(outputArray);
+
+void PrintArray(string[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        Write($"{inArray[i]} ");
+    }
+}
+
+
+string[] CreateNewArray(string[] inArray)
+{
+    int temp = 0;
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (inArray[i].Length <= 3) temp++;
+    }
+    string[] newArray = new string[temp];
+    temp = 0;
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (inArray[i].Length <= 3)
+        {
+            newArray[temp] = inArray[i];
+            temp++;
+        }
+        if (temp == newArray.Length) break;
+    }
+    return newArray;
+
+}
